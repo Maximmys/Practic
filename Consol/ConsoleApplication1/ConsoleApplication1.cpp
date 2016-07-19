31e51f051cc9c14d7af5ff2b89ec1c915021429c
Maximmys <Maximmys@ukr.net>
2016-07-19 21:07:19 +0300

    Wrode pashet dodelaite oformlenie

--- Consol/ConsoleApplication1/ConsoleApplication1.cpp
+++ Consol/ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,12 +4,54 @@
 #include <windows.h>
 #include <iostream> 
 #include <string>
-
+#include <TlHelp32.h>
 using namespace std;
 
-//bool OPEN();
-//bool LIST();
-//bool REMOVE();
+
+void OPEN()
+{
+	wstring s;
+	wcin >> s;
+	LPCWSTR Path = s.c_str();
+	ShellExecute(NULL,L"open", Path, NULL, NULL, SW_SHOWNORMAL);
+}
+void LIST()
+{
+	HANDLE CONST hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	PROCESSENTRY32 peProcessEntry;
+	TCHAR szBuff[1024];
+	DWORD dwTemp;
+	HANDLE CONST hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (INVALID_HANDLE_VALUE == hSnapshot) {
+		return;
+	}
+	peProcessEntry.dwSize = sizeof(PROCESSENTRY32);
+	Process32First(hSnapshot, &peProcessEntry);
+	do {
+		wsprintf(szBuff, L" %08X %s \r\n", peProcessEntry.th32ProcessID, peProcessEntry.szExeFile);
+		WriteConsole(hStdOut, szBuff, lstrlen(szBuff), &dwTemp, NULL);
+	} while (Process32Next(hSnapshot, &peProcessEntry));
+
+	CloseHandle(hSnapshot);
+}
+void REMOVE()
+{
+	wstring s;
+	wcin >> s;
+	HANDLE Target = NULL;
+	PROCESSENTRY32 Pc = { sizeof(PROCESSENTRY32) };
+	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+	if (Process32First(hSnapshot, &Pc)) {
+		do {
+			if (!wcscmp(Pc.szExeFile, s.c_str())) {
+				Target = OpenProcess(PROCESS_ALL_ACCESS, TRUE, Pc.th32ProcessID);
+			}
+		} while (Process32Next(hSnapshot, &Pc));
+		TerminateProcess(Target, 0);
+		CloseHandle(Target);
+	}
+}
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
@@ -23,12 +65,11 @@ int main()
 		cout << "3 - для завершение процеса\n";
 		cout << "4 - если вы гуманитарий" << endl;
 		cin >> s;
-		system("cls");
 		switch(s)
 		{
-		case 1: break; //OPEN();
-		case 2:  break; //LIST();
-		case 3: cin >> ss; break; // REMOVE();
+		case 1: OPEN(); break; 
+		case 2: LIST(); break;
+		case 3: REMOVE(); break;
 		case 4: system("shutdown /s"); exit(0); break;
 		default:exit(0);
 		}
